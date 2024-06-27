3506f661a7c5a3272cb12c7d44f6d0a64e5c40bc
chirag wamanacharya <161801971+Chirag-wamanacharya@users.noreply.github.com>
2024-06-27 12:44:45 +0530

    Add files via upload

---
+++ Day 1/chirag.java
@@ -0,0 +1,18 @@
+class chirag {
+	public static void main(String[] args)
+	{
+
+		int i;
+		int j;
+
+
+		for (i = 10; i<=20; i++) {
+		    System.out.println("Table of " + i);
+		    for(j = 1; j <= 10; j++){
+		        System.out.println(i*j);
+		    }
+		    System.out.println("");
+		    
+		}
+	}
+}
