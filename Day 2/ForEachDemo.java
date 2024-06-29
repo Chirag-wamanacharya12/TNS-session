5c6eb6e9ba981d04f6373ff3e4370ffd81647f17
chirag wamanacharya <161801971+Chirag-wamanacharya@users.noreply.github.com>
2024-06-29 13:14:09 +0530

    Add files via upload

---
+++ Day 2/ForEachDemo.java
@@ -0,0 +1,16 @@
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		int[] intarr = { 10, 20, 30, 30, 50 };
+
+		for (int i : intarr) {
+			System.out.println(i);
+		}
+	
+
+	String[] names= {"Sanika", "Ansh","Manish"};
+	for (String i : names) {
+		System.out.println(i);
+	}
+}
+}
